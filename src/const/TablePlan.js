e96fd51773bf4e94c91af8044b740eb180254bb1
Evgeniy Saigachenko <yohanson555@gmail.com>
2022-01-27 14:15:16 +0300

    'untill.' removed from containers names

--- src/const/TablePlan.js
+++ src/const/TablePlan.js
@@ -6,7 +6,7 @@ export const MIN_PLAN_WIDTH = 400;
 export const MIN_PLAN_HEIGHT = 400;
 export const SIZE_INPUT_STEP = 10;
 
-export const TABLE_PLAN_ITEMS_FIELD_NAME = "untill.air_table_plan_item";
+export const TABLE_PLAN_ITEMS_FIELD_NAME = "air_table_plan_item";
 
 export const TABLE_PLAN_NUMBER_ACCESSOR = "number_accessor";
 export const TABLE_PROP_WIDTH_ACCESSOR = "width_accessor";
